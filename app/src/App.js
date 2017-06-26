04899171b166fe137d7c9e6891037f3320084060
Raul <raullarosa.personal@gmail.com>
2017-06-26 02:14:00 -0400

    implementing firebase

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import fire from './fire'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { }
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ app/src/js/fire.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyAnNPMb6SVXVB3NkJ7B5vxE3NrrBeXeqbo",
+    authDomain: "pi-namite.firebaseapp.com",
+    databaseURL: "https://pi-namite.firebaseio.com"
+}
+var fire = firebase.initializeApp(config)
+export default fire
