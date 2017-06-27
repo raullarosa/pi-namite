ae2549db74acbc0470cdc08b40fa9198cc95af51
Raul <raullarosa.personal@gmail.com>
2017-06-27 18:40:33 -0400

    importing dummy data for presenation

--- app/src/components/Table.js
+++ app/src/components/Table.js
@@ -11,102 +11,263 @@ class Table extends Component {
 			<table className="striped responsive-table">
 				<thead>
 					<tr>
-						<th>Name</th>
-						<th>Item Name</th>
-						<th>Item Price</th>
+						<th>SKU</th>
+						<th>Description</th>
+						<th>Category</th>
+						<th>Buidling</th>
+						<th>Inventory</th>
+						<th>Value</th>
+						<th>Min Inventory</th>
+						<th>Max Inventory</th>
+						<th>Units per Pkg</th>
+						<th>Unit Rate</th>
 					</tr>
 				</thead>
 
 				<tbody>
+					{/*Building A*/}
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>375196</td>
+						<td>CALCULATOR,FINANCIAL,HP12C</td>
+						<td>Calculator</td>
+						<td>A</td>
+						<td>1</td>
+						<td>$84.99</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$84.99</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
+						<td>364255</td>
+						<td>DESKPAD,2017,MONTHLY,22X17</td>
+						<td>Calendar</td>
+						<td>A</td> 
+						<td>8</td>
+						<td>$79.92</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$9.99</td>
 					</tr>
 					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>308957</td>
+						<td>CLIP,BINDER,LARGE,2IN,12BX</td>
+						<td>Clip</td>
+						<td>A</td> 
+						<td>8</td>
+						<td>$51.04</td>
+						<td>4</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$0.53</td>
 					</tr>
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>207464</td>
+						<td>FOLDER,FSTR,LGL,1/3CT,50BX,MNA</td>
+						<td>Folder</td>
+						<td>A</td> 
+						<td>21</td>
+						<td>$2,266.77</td>
+						<td>10</td>
+						<td>24</td>
+						<td>3</td>
+						<td>$35.98</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
+						<td>259251</td>
+						<td>MARKER,CHISEL TIP,EXPO,DZ,BLK</td>
+						<td>Marker</td>
+						<td>A</td> 
+						<td>7</td>
+						<td>$212.89</td>
+						<td>3</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$2.53</td>
+					</tr>
+					{/*Building B*/}
+					<tr>
+						<td>375196</td>
+						<td>CALCULATOR,FINANCIAL,HP12C</td>
+						<td>Calculator</td>
+						<td>B</td>
+						<td>1</td>
+						<td>$84.99</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$84.99</td>
 					</tr>
 					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>364255</td>
+						<td>DESKPAD,2017,MONTHLY,22X17</td>
+						<td>Calendar</td>
+						<td>B</td> 
+						<td>8</td>
+						<td>$79.92</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$9.99</td>
 					</tr>
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>308957</td>
+						<td>CLIP,BINDER,LARGE,2IN,12BX</td>
+						<td>Clip</td>
+						<td>B</td> 
+						<td>8</td>
+						<td>$51.04</td>
+						<td>4</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$0.53</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
+						<td>207464</td>
+						<td>FOLDER,FSTR,LGL,1/3CT,50BX,MNA</td>
+						<td>Folder</td>
+						<td>B</td> 
+						<td>21</td>
+						<td>$2,266.77</td>
+						<td>10</td>
+						<td>24</td>
+						<td>3</td>
+						<td>$35.98</td>
 					</tr>
 					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>259251</td>
+						<td>MARKER,CHISEL TIP,EXPO,DZ,BLK</td>
+						<td>Marker</td>
+						<td>B</td> 
+						<td>7</td>
+						<td>$212.89</td>
+						<td>3</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$2.53</td>
+					</tr>
+					{/*Building C*/}
+					<tr>
+						<td>375196</td>
+						<td>CALCULATOR,FINANCIAL,HP12C</td>
+						<td>Calculator</td>
+						<td>C</td>
+						<td>1</td>
+						<td>$84.99</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$84.99</td>
 					</tr>
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>364255</td>
+						<td>DESKPAD,2017,MONTHLY,22X17</td>
+						<td>Calendar</td>
+						<td>C</td> 
+						<td>8</td>
+						<td>$79.92</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$9.99</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
+						<td>308957</td>
+						<td>CLIP,BINDER,LARGE,2IN,12BX</td>
+						<td>Clip</td>
+						<td>C</td> 
+						<td>8</td>
+						<td>$51.04</td>
+						<td>4</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$0.53</td>
 					</tr>
 					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>207464</td>
+						<td>FOLDER,FSTR,LGL,1/3CT,50BX,MNA</td>
+						<td>Folder</td>
+						<td>C</td> 
+						<td>21</td>
+						<td>$2,266.77</td>
+						<td>10</td>
+						<td>24</td>
+						<td>3</td>
+						<td>$35.98</td>
 					</tr>
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>259251</td>
+						<td>MARKER,CHISEL TIP,EXPO,DZ,BLK</td>
+						<td>Marker</td>
+						<td>C</td> 
+						<td>7</td>
+						<td>$212.89</td>
+						<td>3</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$2.53</td>
+					</tr>
+					{/*Building D*/}
+					<tr>
+						<td>375196</td>
+						<td>CALCULATOR,FINANCIAL,HP12C</td>
+						<td>Calculator</td>
+						<td>D</td>
+						<td>1</td>
+						<td>$84.99</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$84.99</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
+						<td>364255</td>
+						<td>DESKPAD,2017,MONTHLY,22X17</td>
+						<td>Calendar</td>
+						<td>D</td> 
+						<td>8</td>
+						<td>$79.92</td>
+						<td>1</td>
+						<td>10</td>
+						<td>1</td>
+						<td>$9.99</td>
 					</tr>
 					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>308957</td>
+						<td>CLIP,BINDER,LARGE,2IN,12BX</td>
+						<td>Clip</td>
+						<td>D</td> 
+						<td>8</td>
+						<td>$51.04</td>
+						<td>4</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$0.53</td>
 					</tr>
 					<tr>
-						<td>Alvin</td>
-						<td>Eclair</td>
-						<td>$0.87</td>
+						<td>207464</td>
+						<td>FOLDER,FSTR,LGL,1/3CT,50BX,MNA</td>
+						<td>Folder</td>
+						<td>D</td> 
+						<td>21</td>
+						<td>$2,266.77</td>
+						<td>10</td>
+						<td>24</td>
+						<td>3</td>
+						<td>$35.98</td>
 					</tr>
 					<tr>
-						<td>Alan</td>
-						<td>Jellybean</td>
-						<td>$3.76</td>
-					</tr>
-					<tr>
-						<td>Jonathan</td>
-						<td>Lollipop</td>
-						<td>$7.00</td>
+						<td>259251</td>
+						<td>MARKER,CHISEL TIP,EXPO,DZ,BLK</td>
+						<td>Marker</td>
+						<td>D</td> 
+						<td>7</td>
+						<td>$212.89</td>
+						<td>3</td>
+						<td>10</td>
+						<td>12</td>
+						<td>$2.53</td>
 					</tr>
 				</tbody>
 			</table>
