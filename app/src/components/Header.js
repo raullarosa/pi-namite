5e92ac0e4dce12a7c9577ef2332c045204e4198f
Raul <raullarosa.personal@gmail.com>
2017-06-27 18:48:19 -0400

    disabling dropdown for presenation

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -11,14 +11,12 @@ class Header extends Component {
 			<nav>
 				<div className="nav-wrapper">
 					<a href="/" className="brand-logo">pi-namite</a>
-            
 					<ul id="nav-mobile" className="right hide-on-med-and-down">
-            
-    
-            
-
-                                    
-                       /* <li><a href="#stats">Stats</a></li>
+						<li><a href="#stats">Stats</a></li>
+						<li><a href="#history">History</a></li>
+						<li><a href="#login">Login</a></li>
+					</ul>
+					{/* <li><a href="#stats">Stats</a></li>
 						<li><a href="#history">History</a></li>
 						<li><a href="#login">Login</a></li>
                         <button onclick="myFunction()" class="dropbtn">Building</button>
@@ -26,19 +24,11 @@ class Header extends Component {
                                 <a href="#buildingA">A</a>
                                 <a href="#buildingB">B</a>
                                 <a href="#buildingC">C</a>
-                                </div>*/
-                                                
-
-					</ul>
+                                </div>*/}
 				</div>
-            </nav>
-            
-			
+			</nav>
 		)
 	}
 }
-
-
-
 
 export default Header
