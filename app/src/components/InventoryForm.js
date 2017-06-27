d7567a1be2a81948d7ec4edc5ea92456649fea42
Raul <raullarosa.personal@gmail.com>
2017-06-27 13:26:35 -0400

    implementing modal structure

--- app/src/components/InventoryForm.js
+++ app/src/components/InventoryForm.js
@@ -8,7 +8,22 @@ class InventoryForm extends Component {
 	}
 	render() {
 		return (
-			<div></div>
+			<div>
+				<div class="fixed-action-btn">
+					<a class="btn-floating btn-large red" href="#inventory-form">
+						<i class="large material-icons">mode_edit</i>
+					</a>
+				</div>
+				<div id="inventory-form" class="modal bottom-sheet">
+					<div class="modal-content">
+						<h4>Modal Header</h4>
+						<p>A bunch of text</p>
+					</div>
+					<div class="modal-footer">
+						<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+					</div>
+				</div>
+			</div>
 		)
 	}
 }
