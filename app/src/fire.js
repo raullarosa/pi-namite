d86b2bc98eddc7632b34e4aea168fd64770f3460
thomasr2013 <thomasr2013@fau.edu>
2017-06-27 18:04:50 -0400

    dropdownmenu

--- app/src/fire.js
+++ app/src/fire.js
@@ -7,11 +7,31 @@ var config = {
 }
 var fire = firebase.initializeApp(config)
 
+//create element
+const ulist = document.getElementById('list');
+
 //create reference
 const dfRefObject = fire.database().ref().child('items');
 
+
+const dblist = dfRefObject.child('364255')
+
 //sync object changes
 dfRefObject.on('value', snap =>console.log(snap.val())); ///{preObject.innerText = JSON.stringify(snap.val(), 3)});
+
+
+
+// sync list changes
+//console.log(snap.val())});
+
+
+dblist.on('value', snap => {
+
+//const li = document.createElement('li');
+      //    li.innerText = snap.val();
+//ulist.appendChild(li);
+});
+
 
 export default fire
 
